9f613135c6ed39c66f65a82f3b23de46ad9a0e74
Pravendra Singh <hackpravj@gmail.com>
2014-10-22 17:45:46 +0530

    utils : add Map and Hex_val methods
    > Map : map a value from a number range to different number range
    > Hex_val : convert a substring of Hexadecimal hash to its decimal value

--- utils/utils.go
+++ utils/utils.go
@@ -3,6 +3,7 @@ package utils
 import (
     "fmt"
     "crypto/sha1"
+    "strconv"
 )
 
 func Hash(s string) string {
@@ -12,4 +13,22 @@ func Hash(s string) string {
     hash := h.Sum(nil)
 
     return fmt.Sprintf("%x", hash)
+}
+
+func Map(value, a_min, a_max, b_min, b_max float64) int {
+    a_range := a_max - a_min
+    b_range := b_max - b_min
+
+    return int(b_max - (a_max - value) * (b_range/a_range))
+}
+
+func Hex_val(str string, index, length int) int {
+    hex_str := str[index:index+length]
+
+    hex_val, err := strconv.ParseInt(hex_str, 16, 0)
+    if err != nil {
+        panic(err)
+    }
+
+    return int(hex_val)
 }
