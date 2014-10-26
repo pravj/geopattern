4631a8f1dba7adee84d545f5f54294f0ff779104
Pravendra Singh <hackpravj@gmail.com>
2014-10-26 20:17:40 +0530

    documentation : shapes and utils packages

--- shapes/shapes.go
+++ shapes/shapes.go
@@ -1,3 +1,4 @@
+// Package shapes implements some geometric shapes to be used in project
 package shapes
 
 import (
@@ -7,6 +8,7 @@ import (
 	"math"
 )
 
+// Build_octagon returns string representing an octagon shape
 func Build_octagon(square_size float64) string {
 	s := square_size
 	c := 0.33 * s
@@ -14,26 +16,31 @@ func Build_octagon(square_size float64) string {
 	return fmt.Sprintf("%v,0,%v,0,%v,%v,%v,%v,%v,%v,%v,%v,0,%v,0,%v,%v,0", c, s-c, s, c, s, s-c, s-c, s, c, s, s-c, c, c)
 }
 
+// Build_triangle returns string representing a triangle shape
 func Build_triangle(side_length, height float64) string {
 	half_width := side_length / 2
 
 	return fmt.Sprintf("%v,0,%v,%v,0,%v,%v,0", half_width, side_length, height, height, half_width)
 }
 
+// Build_diamond returns string representing a diamond shape
 func Build_diamond(width, height float64) string {
 	return fmt.Sprintf("%v,0,%v,%v,%v,%v,0,%v", width/2, width, height/2, width/2, height, height/2)
 }
 
+// Build_right_triangle returns string representing a right angle triangle shape
 func Build_right_triangle(side_length float64) string {
 	return fmt.Sprintf("0,0,%v,%v,0,%v,0,0", side_length, side_length, side_length)
 }
 
+// Build_rotated_triangle returns string representing a rotated triangle shape
 func Build_rotated_triangle(side_length, width float64) string {
 	half_height := side_length / 2
 
 	return fmt.Sprintf("0,0,%v,%v,0,%v,0,0", width, half_height, side_length)
 }
 
+// Build_hexagon returns string representing a hexagon shape
 func Build_hexagon(side_length float64) string {
 	c := side_length
 	a := c / 2
@@ -42,6 +49,7 @@ func Build_hexagon(side_length float64) string {
 	return fmt.Sprintf("0,%v,%v,0,%v,0,%v,%v,%v,%v,%v,%v,0,%v", b, a, a+c, 2*c, b, a+c, 2*b, a, 2*b, b)
 }
 
+// Build_chevron returns string representing a chevron shape
 func Build_chevron(width, height float64) [2]string {
 	e := height * 0.66
 	var elements [2]string
@@ -52,6 +60,7 @@ func Build_chevron(width, height float64) [2]string {
 	return elements
 }
 
+// Build_plus returns string representing an plus shape
 func Build_plus(square_size float64) [2]string {
 	var elements [2]string
 
@@ -61,6 +70,7 @@ func Build_plus(square_size float64) [2]string {
 	return elements
 }
 
+// Draw_inner_mosaic_tile returns string representing an inner mosaic tile shape
 func Draw_inner_mosaic_tile(s *svg.SVG, x, y, triangle_size float64, values [2]float64) {
 	triangle := Build_right_triangle(triangle_size)
 	opacity := utils.Opacity(values[0])
@@ -93,6 +103,7 @@ func Draw_inner_mosaic_tile(s *svg.SVG, x, y, triangle_size float64, values [2]f
 	s.Polyline(triangle, utils.Merge(styles, style))
 }
 
+// Draw_outer_mosaic_tile returns string representing an outer mosaic tile shape
 func Draw_outer_mosaic_tile(s *svg.SVG, x, y, triangle_size, value float64) {
 	opacity := utils.Opacity(value)
 	fill := utils.Fill_color(value)
--- utils/utils.go
+++ utils/utils.go
@@ -1,3 +1,5 @@
+// Package utils provide utility functions that helps
+// development across the project.
 package utils
 
 import (
@@ -6,8 +8,10 @@ import (
 	"strconv"
 )
 
+// constants representing stroke, fill's opacity and color,
+// base color, minimum and maximum opacity value
 const (
-	BASE_COLOR       string  = "#933c3c"
+	BASE_COLOR       string  = "#9e2c7b"
 	STROKE_COLOR     string  = "#000"
 	STROKE_OPACITY   float64 = 0.02
 	FILL_COLOR_DARK  string  = "#222"
@@ -16,6 +20,7 @@ const (
 	OPACITY_MAX      float64 = 0.15
 )
 
+// Hash returns SHA-1 encryption of a string
 func Hash(s string) string {
 	h := sha1.New()
 	h.Write([]byte(s))
@@ -25,6 +30,7 @@ func Hash(s string) string {
 	return fmt.Sprintf("%x", hash)
 }
 
+// Map returns respective value of a number from a range to different range
 func Map(value, a_min, a_max, b_min, b_max float64) float64 {
 	a_range := a_max - a_min
 	b_range := b_max - b_min
@@ -32,6 +38,7 @@ func Map(value, a_min, a_max, b_min, b_max float64) float64 {
 	return (b_max - (a_max-value)*(b_range/a_range))
 }
 
+// Hex_val returns decimal representation of a substring of a hexa decimal string
 func Hex_val(str string, index, length int) float64 {
 	hex_str := str[index : index+length]
 
@@ -43,6 +50,7 @@ func Hex_val(str string, index, length int) float64 {
 	return float64(hex_val)
 }
 
+// Merge merges two 'map' objects and returns the resultant object
 func Merge(map_a map[string]interface{}, map_b map[string]interface{}) map[string]interface{} {
 	for k, v := range map_a {
 		map_b[k] = v
@@ -51,10 +59,12 @@ func Merge(map_a map[string]interface{}, map_b map[string]interface{}) map[strin
 	return map_b
 }
 
+// Opacity returns opacity value in a particular range
 func Opacity(value float64) float64 {
 	return Map(value, 0, 15, OPACITY_MIN, OPACITY_MAX)
 }
 
+// Fill_color returns string to be used for fill color
 func Fill_color(value float64) string {
 	if int(value)%2 == 0 {
 		return FILL_COLOR_LIGHT
